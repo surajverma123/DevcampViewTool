b73f37cebf5c8442c8bef081813dfd83956ccef2
Suraj Verma <sv1009876@gmail.com>
2018-12-24 16:14:16 +0530

    Add copyright helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,4 @@ class ApplicationController < ActionController::Base
     include SetSource
     include CurrentUserConcern
     include DefaultPageContent
-
-    before_action :set_copyright
-
-    def set_copyright
-        @copyright = DevcampViewToolToolVerma::Renderer.copyright "Suraj Verma", "All right reservered"
-    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
             content_tag(:p, greeting, class: "source-greeting")
         end
     end
+
+    def copyright_generator
+        DevcampViewToolToolVerma::Renderer.copyright "Suraj Verma", "All right reservered"
+    end
 end
