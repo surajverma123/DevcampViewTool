6548ee84b99288a51591129375f8904de9565265
Suraj Verma <sv1009876@gmail.com>
2018-12-27 21:08:39 +0530

    Integrated twitter on tech-news

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,17 @@
 class PagesController < ApplicationController
   def home
+
   end
 
   def about
+
   end
 
   def contact
+
+  end
+
+  def tech_news
+    @tweets = SocialTool.twitter_search
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about-me', to: "pages#about"
   get 'contact', to: "pages#contact"
+  get 'tech-news', to: "pages#tech_news"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
