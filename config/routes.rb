a4de1a3c4bdd60672323d1f5934deef97f3820fa
Suraj Verma <sv1009876@gmail.com>
2018-12-28 16:11:04 +0530

    created a comment broadcast, configure a actioncable

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :blogs do
     member do
@@ -15,6 +14,7 @@ Rails.application.routes.draw do
   get 'about-me', to: "pages#about"
   get 'contact', to: "pages#contact"
   get 'tech-news', to: "pages#tech_news"
+  mount ActionCable.server => '/cable'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
